b689e355f06f21420dee91f3c4867be38746720f
Matthew Brown <52255000+mbrown98@users.noreply.github.com>
2022-12-01 21:19:43 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Hi</Text>
+    </SafeAreaView>
+  );
+};
+
+// const styles = StyleSheet.create({
+// });
+
+export default App;
+
+{
+  /* <AuthProvider>
+<AppBootstrap>
+    <SettingsProvider>
+        <Navigator />
+    </SettingsProvider>
+</AppBootstrap>
+</AuthProvider> */
+}
